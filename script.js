d848e3a94c318136e74a1c40822b2cc399bc5d40
Suchithra2505 <147540982+Suchithra2505@users.noreply.github.com>
2023-12-29 17:13:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,14 +3,17 @@ function handleInput(input) {
             if (input.value.length === 1) {
                 // Move focus to the next input
                 const nextInput = input.nextElementSibling;
+				
                 if (nextInput) {
-                    nextInput.focus();
+                    //nextInput.focus();
+					setTimeout(() => nextInput.focus(), 100);
                 }
             } else if (input.value.length === 0) {
                 // Move focus to the previous input on backspace
                 const prevInput = input.previousElementSibling;
                 if (prevInput) {
-                    prevInput.focus();
+                   // prevInput.focus();
+					setTimeout(() => prevInput.focus(), 100);
                 }
             }
         }
