9aacdcfa486f2a615267cf8006788c98abc603e1
Suchithra2505 <147540982+Suchithra2505@users.noreply.github.com>
2023-12-29 16:50:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,16 @@
 //your JS code here. If required.
-const codeInputs = document.querySelectorAll('.code');
-
-codeInputs.forEach((input, index) => {
-    input.addEventListener('input', (event) => {
-        if (event.data !== null) {
-            if (index < codeInputs.length - 1) {
-                codeInputs[index + 1].focus();
+function handleInput(input) {
+            if (input.value.length === 1) {
+                // Move focus to the next input
+                const nextInput = input.nextElementSibling;
+                if (nextInput) {
+                    nextInput.focus();
+                }
+            } else if (input.value.length === 0) {
+                // Move focus to the previous input on backspace
+                const prevInput = input.previousElementSibling;
+                if (prevInput) {
+                    prevInput.focus();
+                }
             }
         }
-    });
-
-    input.addEventListener('keydown', (event) => {
-        if (event.key === 'Backspace' && index > 0) {
-            codeInputs[index - 1].focus();
-            codeInputs[index].value = '';
-        }
-    });
-});
